bc443f84cfe5aec1b00de3a4ca8e87644b028939
Andres Agudelo <andresaagudelocartagena0@gmail.com>
2025-11-21 16:36:12 -0500

    cartagena

---
+++ Turis-Travel/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Turis_Travel.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            var userId = HttpContext.Session.GetInt32("Id");
+
+            if (userId == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            ViewBag.Nombre = HttpContext.Session.GetString("Nombre");
+            ViewBag.Rol = HttpContext.Session.GetInt32("IdRol");
+            return View();
+        }
+
+    }
+}
+
--- Turis-Travel/Controllers/LoginController.cs
+++ Turis-Travel/Controllers/LoginController.cs
@@ -13,11 +13,13 @@ namespace Turis_Travel.Controllers
             _usuarioBD = new UsuarioBD();
         }
 
+        // GET: /Login/
         public IActionResult Index()
         {
             return View();
         }
 
+        // POST: /Login/
         [HttpPost]
         public IActionResult Index(string correo, string clave)
         {
@@ -34,6 +36,21 @@ namespace Turis_Travel.Controllers
             HttpContext.Session.SetString("Nombre", u.Nombre);
             HttpContext.Session.SetInt32("IdRol", u.IdRol);
 
+            // Redirección según el rol
+            if (u.IdRol == 1) // Administrador
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else if (u.IdRol == 2) // Empleado
+            {
+                return RedirectToAction("Index", "Empleado");
+            }
+            else if (u.IdRol == 3) // Cliente
+            {
+                return RedirectToAction("Index", "Cliente");
+            }
+
+            // Si no coincide ningún rol
             return RedirectToAction("Index", "Home");
         }
 
@@ -44,3 +61,5 @@ namespace Turis_Travel.Controllers
         }
     }
 }
+
+
